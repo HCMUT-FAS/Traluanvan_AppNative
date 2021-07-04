947f4f5bcb4e8304f6b3f56c90a98964fe4e731d
DanhYYR <Danh0989@gmail.com>
2021-07-04 19:53:46 +0700

    Demo app ver 1.0

---
+++ Luanvanapp/app/src/main/java/com/example/luanvanapp/Form_thong_tin.java
@@ -0,0 +1,14 @@
+package com.example.luanvanapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_thong_tin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_thong_tin);
+    }
+}
